965f73fdb34e06255d0db1a48117b82141f5f465
Anna Dolan <anna.dolan@gmail.com>
2017-02-27 16:17:40 -0700

    Send data to hot reads

--- app/assets/javascripts/mark-read.js
+++ app/assets/javascripts/mark-read.js
@@ -5,12 +5,15 @@ $(document).ready(function(){
   $('#links-list').on('click', 'button.mark-read', function(){
     var $this = $(this);
     var linkId = $this.parents('.link').data('id');
+    var linkUrl = $this.parent().siblings('.link-url').text()
 
     $.ajax({
       url: '/api/v1/links/' + linkId,
       method: 'PATCH',
       data: {read: true}
-    }).then( updateRead($this) );
+    })
+    .then( updateRead($this) )
+    .then( sendToHotReads(linkUrl) );
   })
 
   $('#links-list').on('click', 'button.mark-unread', function(){
@@ -39,4 +42,14 @@ function updateUnread($thing) {
   $thing.parent().siblings('.link_read').text("Read: false")
   $thing.parent().siblings('.link-title').removeClass('read')
   $thing.parent().siblings('.link-url').removeClass('read')
+}
+
+function sendToHotReads(url) {
+  $.ajax({
+    url: "https://dry-stream-75456.herokuapp.com/api/v1/links/",
+    method: "POST",
+    data: {
+          link: { url: url }
+        }
+})
 }
