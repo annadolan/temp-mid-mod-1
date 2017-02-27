5705d686222ccf27b16222c5eeb0fc259f1f839f
Anna Dolan <anna.dolan@gmail.com>
2017-02-27 14:42:57 -0700

    Classes and text toggle on click

--- app/assets/javascripts/mark-read.js
+++ app/assets/javascripts/mark-read.js
@@ -11,5 +11,24 @@ $(document).ready(function(){
       method: 'PATCH',
       data: {read: true}
     });
+
+    $this.text("Mark as Unread")
+    $this.toggleClass('mark-read mark-unread')
+    $this.parent().siblings('.link_read').text("true")
+  })
+
+  $('#links-list').on('click', 'button.mark-unread', function(){
+    var $this = $(this);
+    var linkId = $this.parents('.link').data('id');
+
+    $.ajax({
+      url: '/api/v1/links/' + linkId,
+      method: 'PATCH',
+      data: {read: false}
+    });
+
+    $this.text("Mark as Unread")
+    $this.toggleClass('mark-unread mark-read')
+    $this.parent().siblings('.link_read').text("false")
   })
 })
--- spec/requests/api/v1/link_request_spec.rb
+++ spec/requests/api/v1/link_request_spec.rb
@@ -1,12 +1,21 @@
 require 'rails_helper'
 
 describe "PATCH api/v1/links/:id" do
-  it "updates the specified link", :vcr do
+  it "updates the specified link to read", :vcr do
     link = create(:link)
     headers = {'CONTENT-TYPE' => 'application/json'}
     patch "/api/v1/links/#{link.id}", headers: headers, params: { read: true }.to_json
     expect(response).to be_success
     link = JSON.parse(response.body)
     expect(link["read"]).to eq(true)
+  end
+
+  it "updates the specified link to read", :vcr do
+    link = create(:link, read: true)
+    headers = {'CONTENT-TYPE' => 'application/json'}
+    patch "/api/v1/links/#{link.id}", headers: headers, params: { read: false }.to_json
+    expect(response).to be_success
+    link = JSON.parse(response.body)
+    expect(link["read"]).to eq(false)
   end
 end
