45adb2bb4bebf7aeb950c95bf99e77297b189cf5
Anna Dolan <anna.dolan@gmail.com>
2017-02-27 18:35:31 -0700

    Fix route

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :links, only: [:update, :create]
-      resources :reads, only: [:create]
+      resources :read_links, only: [:create]
     end
   end
 end
